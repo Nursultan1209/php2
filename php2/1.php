3833964b0e25859339506d09e61c290d2db3f1f7
Nursultan1209 <99520116+Nursultan1209@users.noreply.github.com>
2022-04-02 14:51:45 +0600

    Update 1.php

--- php2/1.php
+++ php2/1.php
@@ -1,7 +1,7 @@
 <?php
     $a = 5;
     $b = 7;
-    if (($a>=1 && $a<12) || ($b>6 && $B<14))
+    if (($a>=1 && $a<12) || ($b>6 && $b<14))
         echo "True \r\n";
     else
         echo "False";
